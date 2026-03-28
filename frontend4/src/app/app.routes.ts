24afb2bde7b6ba0e874f1020ab8307eb4b5152df
tithrite <TITHRITE.AITADDI.ESTN24@ump.ac.ma>
2026-03-28 20:44:16 +0100

    Update app.routes.ts

--- frontend4/src/app/app.routes.ts
+++ frontend4/src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
 
-/**
- * Routes de l'application - application mono-page (SPA)
- * Toute la logique est dans le composant principal AppComponent
- */
+
 export const routes: Routes = [];
